5b3bfadbcbc79d8ccd90f11ac72f530a7c196353
Per Gunnar Paulsen <58368467+pergpau@users.noreply.github.com>
2020-05-20 20:12:38 +0200

    Update init.php

--- app/init.php
+++ app/init.php
@@ -2,9 +2,9 @@
 session_start();
 
     // データベースに接続
-    $dsn = getenv('DB_HOST')
-    $user = getenv('DB_USER')
-    $password = getenv('DB_PASSWORD')
+    $dsn = getenv('DB_HOST');
+    $user = getenv('DB_USER');
+    $password = getenv('DB_PASSWORD');
     $driver_options = array(
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => false,
