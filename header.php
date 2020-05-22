367617c0a0d99c1a9ab70fd69a222a8da46e8b53
Per Gunnar Paulsen <58368467+pergpau@users.noreply.github.com>
2020-05-22 18:32:04 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 
 <head>
 	<meta charset=“UTF-8”>
+	<meta name=viewport content="width=device-width,initial-scale=1">
+	<link rel=stylesheet href=https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css
+        integrity=sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh crossorigin=anonymous>
 	<title>ノルウェー語・日本語辞書</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
