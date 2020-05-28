ac0864f73050b86540d2a43995bde54daa4097ab
Per Gunnar Paulsen <58368467+pergpau@users.noreply.github.com>
2020-05-28 18:17:18 +0200

    Update header.php

--- header.php
+++ header.php
@@ -6,12 +6,12 @@
 	<meta name=viewport content="width=device-width,initial-scale=1">
 	<link rel=stylesheet href=https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css
         integrity=sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh crossorigin=anonymous>
-	<title>ノルウェー語・日本語辞書</title>
+	<title>jisho.no ノルウェー語・日本語辞書</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 
 <body>
-	<h1>ノルウェー語・日本語辞書</h1>
+	<h1>jisho.no ノルウェー語・日本語辞書 (beta)</h1>
 	<div class="stitch">
 		<p>検索ワード (半角英数字)</p>
 		<form action="search.php" method="GET">
