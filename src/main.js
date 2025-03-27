1943ffc3397b737c177cec8a25bef01f5ad5af5c
Per Gunnar Paulsen <pergpau@gmail.com>
2025-03-27 19:36:15 +0100

    fix jquery

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import $ from 'jquery';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/css/main.css';
 import i18n from './i18n';
 import router from './router';
 
-window.$ = $ 
 
 createApp(App).use(router).use(i18n).mount('#app')
